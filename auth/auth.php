eab901c414f112db93759bb51a519f2cc77f427c
anass17 <anassboutaib2018@gmail.com>
2024-12-19 01:19:35 +0100

    Added function to create new users

--- auth/auth.php
+++ auth/auth.php
@@ -3,21 +3,35 @@
 require_once "../connect/db-connect.php";
 require_once "JWT.php";
 
+function createJWTCookie($id, $email, $role) {
+    $jwt = createJWT($id, $email, $role);
+
+    $options = [
+        'expires' => time() + (60 * 60 * 24),
+        'path' => '/',
+        'domain' => 'localhost',
+        'secure' => false,
+        'httponly' => true,
+        'samesite' => 'Strict'
+    ];
+
+    setcookie('token', $jwt, $options);
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // Get values
 
     $form_type = isset($_POST["form-type"]) ? $_POST["form-type"] : "";
-    $email = isset($_POST["email"]) ? $_POST["email"] : "";
-    $password = isset($_POST["password"]) ? $_POST["password"] : "";
-
-    // validate with PHP
-
-
-
-    // Check if the user exists in the database
 
     if ($form_type == "login") {
+
+        // Get values
+
+        $email = isset($_POST["email"]) ? $_POST["email"] : "";
+        $password = isset($_POST["password"]) ? $_POST["password"] : "";
+
+        // Check if user exists
+
         $stmt = $conn -> prepare("select email, password, user_id, role_name from users join roles on users.user_id = roles.role_id where email = ?");
 
         $stmt -> bind_param("s", $email);
@@ -28,28 +42,56 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $row = $result -> fetch_assoc();
 
             if ($row) {
-                print_r($row);
 
                 if (password_verify($password, $row["password"])) {
-                    
-                    $jwt = createJWT($row['user_id'], $row["email"], $row["role_name"]);
-
-                    $options = [
-                        'expires' => time() + (60 * 60 * 24),
-                        'path' => '/',
-                        'domain' => 'localhost',
-                        'secure' => false,
-                        'httponly' => true,
-                        'samesite' => 'Strict'
-                    ];
-
-                    setcookie('token', $jwt, $options);
+                    createJWTCookie($row['user_id'], $row["email"], $row["role_name"]);
                 }
             } else {
                 echo 'Empty Result';
             }
         } else {
             echo 'Error';
+        }
+    } else if ($form_type == "signup") {
+
+        // Get values
+
+        $first_name = isset($_POST["first_name"]) ? $_POST["first_name"] : "";
+        $last_name = isset($_POST["last_name"]) ? $_POST["last_name"] : "";
+        $email = isset($_POST["email"]) ? $_POST["email"] : "";
+        $password = isset($_POST["password"]) ? $_POST["password"] : "";
+        $confirm_password = isset($_POST["confirm-password"]) ? $_POST["confirm-password"] : "";
+
+        // Check if user exists
+
+        $stmt = $conn -> prepare("select email from users where email = ?");
+
+        $stmt -> bind_param("s", $email);
+
+        if ($stmt -> execute()) {
+            $result = $stmt -> get_result();
+
+            $row = $result -> fetch_assoc();
+
+            if ($row) {
+                echo 'This user already exists';
+            } else {
+                $first_name = ucfirst(strtolower($first_name));
+                $last_name = ucfirst(strtolower($last_name));
+                $password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+                $stmt = $conn -> prepare("INSERT INTO users (email, password, first_name, last_name, user_role) VALUES (?, ?, ?, ?, 2);");
+
+                $stmt -> bind_param("ssss", $email, $password_hash, $first_name, $last_name);
+
+                if($stmt -> execute()) {
+                    $lastInsertedId = $conn -> insert_id;
+                    createJWTCookie($lastInsertedId, $email, "user");
+                    header('Location: ../blogs.php');
+                } else {
+                    echo 'Error';
+                }
+            }
         }
     }
 
--- auth/signup.php
+++ auth/signup.php
@@ -15,7 +15,7 @@
     ?>
     
     <div class="bg-gray-200 flex items-center justify-center h-full">
-        <form action="" method="POST" class="bg-white rounded-lg shadow-lg px-7 py-6 w-full max-w-lg">
+        <form action="auth.php" method="POST" class="bg-white rounded-lg shadow-lg px-7 py-6 w-full max-w-lg">
             <h2 class="text-center mb-7 text-2xl font-bold text-green-500">SIGN UP</h2>
             <div class="form-group mb-5 flex gap-4">
                 <div class="form-input w-full">
@@ -36,15 +36,15 @@
                 <input type="password" placeholder="At least 8 characters" class="border border-gray-300 rounded w-full px-3 py-2" id="password" name="password">
             </div>
             <div class="form-input mb-4">
-                <label for="password" class="block mb-1">Confirm Password</label>
-                <input type="password" placeholder="At least 8 characters" class="border border-gray-300 rounded w-full px-3 py-2" id="password" name="password">
+                <label for="confirm-password" class="block mb-1">Confirm Password</label>
+                <input type="password" placeholder="At least 8 characters" class="border border-gray-300 rounded w-full px-3 py-2" id="confirm-password" name="confirm-password">
             </div>
             <div class="mb-4 flex items-center gap-3">
                 <input type="checkbox" name="agreement" id="agreement" class="relative top-0.5 scale-125">
                 <label for="agreement">I agree to the <a href="#" class="font-semibold text-blue-500">terms and conditions</a></label>
             </div>
             <p class="text-sm text-gray-500">You already have an account? <a href="login.php" class="font-semibold text-blue-500">Log in</a></p>
-            <button type="submit" class="px-4 py-2 rounded bg-green-500 text-white font-semibold mt-5">Sign Up</button>
+            <button type="submit" class="px-4 py-2 rounded bg-green-500 text-white font-semibold mt-5" name="form-type" value="signup">Sign Up</button>
         </form>
     </div>
 
